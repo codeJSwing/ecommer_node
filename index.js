c64f20ddd8032ec5d97de86e6e868303b51a7780
codeJSwing <imlogic20@gmail.com>
2023-03-04 15:06:24 +0900

    node init

---
+++ index.js
@@ -0,0 +1,16 @@
+import express from "express"
+const app = express()
+
+
+
+const port = 1111 //문
+app.listen(port, console.log("서버 started"))
+
+
+
+// 코드 빌드 순서
+// 1. 위에서 아래
+// 2. 괄호는 함수들의 모음을 뜻한다.
+// 3. '.' 은 하위 메소드를 호출한다.
+// 4. equal을 기준으로 왼쪽에서 오른쪽으로 해석
+// 5. , 는 그리고로 해석한다.
